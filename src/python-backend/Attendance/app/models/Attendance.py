fbf20d089cb4dfdda68bbd0ebd92630e44765a70
Santi798 <santiagoandresfetecuapulgarin@gmail.com>
2025-11-15 13:01:37 -0500

    Database startup config for the microservice

---
+++ src/python-backend/Attendance/app/models/Attendance.py
@@ -0,0 +1,18 @@
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Integer, String, Boolean
+from app.db.Database import Base
+
+class Attendance(Base):
+    __tablename__ = "Attendance"
+
+    attendanceID: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+    nameAttendance: Mapped[str] = mapped_column(String(100), nullable=False)
+    emailAttendance: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    contactNumber: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    documentID: Mapped[int] = mapped_column(Integer, nullable=False, index=True)
+    waitlist: Mapped[bool] = mapped_column(Boolean, nullable=False)
+    eventAssistanceID: Mapped[int] = mapped_column(Integer, nullable=False, index=True)
+
+    def __repr__(self) -> str:
+        return f"<Attendance(id={self.attendance_id} name={self.name!r} email={self.email!r})>"
+    
--- src/python-backend/Attendance/main.py
+++ src/python-backend/Attendance/main.py
@@ -1,10 +1,28 @@
+'''
+Main entry poit for the Attendance microservice
+'''
 from fastapi import FastAPI
-from app.api.attendance import router as attendance_router
+from app.api.attendance_end import router as attendance_router
+from contextlib import asynccontextmanager
+from app.db.Database import Database
+import app.models.Attendance # Load the model so it associated table can be created
 
-app = FastAPI()
+@asynccontextmanager
+async def startup(app: FastAPI):
+    '''
+    Create the database tables if needed before starting the app.
+    Dispose the engine on shutDown.
+    '''
+    await Database.init_models()
+    yield
+    # Clean up the ML models and release the resources
+    await Database.dispose_engine()
 
-app.include_router(attendance_router, prefix="/users", tags=["Users"])
+app = FastAPI(lifespan=startup)
+
+app.include_router(attendance_router, prefix="/attendance", tags=["Attendance"])
 
 @app.get("/")
 def health_check():
     return {"status": "ok"}
+
