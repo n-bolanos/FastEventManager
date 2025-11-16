62e4e1ea96429e61250809fc4ea6e18b79850025
Santi798 <santiagoandresfetecuapulgarin@gmail.com>
2025-11-15 21:46:59 -0500

    unique attributes changed

--- src/python-backend/Attendance/app/models/Attendance.py
+++ src/python-backend/Attendance/app/models/Attendance.py
@@ -7,8 +7,8 @@ class Attendance(Base):
 
     attendanceID: Mapped[int] = mapped_column(Integer, primary_key=True, index=True, autoincrement=True)
     nameAttendance: Mapped[str] = mapped_column(String(100), nullable=False)
-    emailAttendance: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
-    contactNumber: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    emailAttendance: Mapped[str] = mapped_column(String(120), nullable=False)
+    contactNumber: Mapped[str] = mapped_column(String(120), nullable=False)
     documentID: Mapped[int] = mapped_column(Integer, nullable=False, index=True)
     waitlist: Mapped[bool] = mapped_column(Boolean, nullable=False)
     eventAssistanceID: Mapped[int] = mapped_column(Integer, nullable=False, index=True)
