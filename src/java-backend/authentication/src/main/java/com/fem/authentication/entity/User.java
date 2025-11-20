7409f3afee81bbf8c211051070542f0fc098d2a8
n-bolanos <nianbolanos@gmail.com>
2025-11-19 21:14:14 -0500

    WIP: entity and repo defined

---
+++ src/java-backend/authentication/src/main/java/com/fem/authentication/entity/User.java
@@ -0,0 +1,24 @@
+package com.fem.authentication.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor @Builder
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long userID;
+
+    @Column(nullable=false)
+    private String name; 
+
+    @Column(nullable=false)
+    private String username; 
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable=false)
+    private String passwordHash;
+}
