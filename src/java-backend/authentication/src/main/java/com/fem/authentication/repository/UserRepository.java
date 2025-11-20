22d5c6e8195ebd192b1831b480cc79de8489a247
n-bolanos <nianbolanos@gmail.com>
2025-11-19 22:40:04 -0500

    Queries added to the repository

--- src/java-backend/authentication/src/main/java/com/fem/authentication/repository/UserRepository.java
+++ src/java-backend/authentication/src/main/java/com/fem/authentication/repository/UserRepository.java
@@ -7,4 +7,8 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long>{
     Optional<User> findByEmail(String email);
     boolean existsByEmail(String email);
+    Optional<User> findById(String userID);
+    boolean existsById(String userID);
+    Optional<User> findByEmailOrUsername(String email, String userID);
+    boolean existsByEmailOrUsername(String email, String userID);
 }
