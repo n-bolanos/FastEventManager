f8085365abaa82aa5d0d3dfcc7352ae47e4aef87
n-bolanos <nianbolanos@gmail.com>
2025-11-20 15:08:51 -0500

    DB creation file, sample data and docker image

---
+++ src/java-backend/authentication/src/main/resources/data.sql
@@ -0,0 +1,8 @@
+USE auth_microservice;
+
+INSERT INTO users(name, username, email, password_hash)
+VALUES ('pruebaa jaja', 'demo1', 'dem@example.com', '$1a$10$hash_goes_here');
+VALUES ('pruebaa jaja', 'demo2', 'deo@example.com', '$2a$10$hash_goes_here');
+VALUES ('pruebaa jaja', 'demo3', 'dmo@example.com', '$3a$10$hash_goes_here');
+VALUES ('pruebaa jaja', 'demo4', 'emo@example.com', '$4a$10$hash_goes_here');
+VALUES ('pruebaa jaja', 'demo5', 'demo@example.com', '$5a$10$hash_goes_here');
---
+++ src/java-backend/authentication/src/main/resources/schema.sql
@@ -0,0 +1,12 @@
+USE auth_microservice;
+
+-- Table storing system users
+CREATE TABLE IF NOT EXISTS user (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    
+    username VARCHAR(100) UNIQUE,
+    email VARCHAR(255) UNIQUE,
+    name VARCHAR(255) UNIQUE,
+    
+    password_hash VARCHAR(255) NOT NULL
+);
