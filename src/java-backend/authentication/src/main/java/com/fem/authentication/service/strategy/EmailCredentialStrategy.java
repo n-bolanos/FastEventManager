9c0379663c059bc5939c1970defa4e4a4316b188
n-bolanos <nianbolanos@gmail.com>
2025-11-20 21:08:18 -0500

    refactor: Minor changes in login and getuserInfo

--- src/java-backend/authentication/src/main/java/com/fem/authentication/service/strategy/EmailCredentialStrategy.java
+++ src/java-backend/authentication/src/main/java/com/fem/authentication/service/strategy/EmailCredentialStrategy.java
@@ -4,7 +4,6 @@ import com.fem.authentication.dto.LoginRequest;
 import com.fem.authentication.entity.User;
 import com.fem.authentication.repository.UserRepository;
 import org.springframework.stereotype.Component;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import java.util.Optional;
