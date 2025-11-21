26c891a9043f6b382fc89b07d46c932e3e7af774
n-bolanos <nianbolanos@gmail.com>
2025-11-21 09:41:43 -0500

    test: new test for verifying invalid tokens.

--- src/java-backend/authentication/src/test/java/com/fem/authentication/util/JwtUtilTest.java
+++ src/java-backend/authentication/src/test/java/com/fem/authentication/util/JwtUtilTest.java
@@ -11,30 +11,35 @@ import org.junit.jupiter.api.Test;
 import com.auth0.jwt.exceptions.TokenExpiredException;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Optional;
+
 public class JwtUtilTest {
-    /**
-     * Tests if JwtUtil generates a valid JWT according to the specified secret
-     */
+
+    @Test
+    void testTokenExpires() throws InterruptedException {
+    JwtUtil jwtUtil = new JwtUtil("secret123", 2);
+
+    // Create a token that expires quickly
+    String token = jwtUtil.generateToken("test");
+    assertDoesNotThrow(() -> jwtUtil.verify(token));
+    Thread.sleep(3000);
+    assertThrows(TokenExpiredException.class, () -> jwtUtil.verify(token));
+    }
+
     @Test
     void testGenerateAndVerifyToken() {
         JwtUtil jwtUtil = new JwtUtil("secret123", null);
         String token = jwtUtil.generateToken("user@example.com");
         assertNotNull(token);
-        DecodedJWT decoded = jwtUtil.verify(token);
+        DecodedJWT decoded = jwtUtil.verify(token).get();       
         assertEquals("user@example.com", decoded.getSubject());
     }
 
     @Test
-    void testtestTokenExpires() throws InterruptedException {
-    JwtUtil jwtUtil = new JwtUtil("secret123", 1);
-
-    // Create a token that expires quickly
-    String token = jwtUtil.generateToken("test");
-    assertDoesNotThrow(() -> jwtUtil.verify(token));
-
-    Thread.sleep(2000);
-
-    assertThrows(TokenExpiredException.class, () -> jwtUtil.verify(token));
-}
-
+    void testErrorForInvalidToken(){
+        JwtUtil jwtUtil = new JwtUtil("secret123", null);
+        String token = jwtUtil.generateToken("user@example.com");
+        token = token.concat("modificacion");
+        assertEquals(Optional.empty(), jwtUtil.verify(token));
+    }
 }
