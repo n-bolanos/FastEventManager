7fad1ca2f6a135ed8438270ba248997ca40c52a9
trimsucne <secoes203@gmail.com>
2025-11-30 21:05:03 -0500

    Update Workshop 3

--- src/web-frontend/FEM_FRONT/src/assets/js/Attendance.js
+++ src/web-frontend/FEM_FRONT/src/assets/js/Attendance.js
@@ -25,9 +25,7 @@ export async function confirm(body, query){
     try {
         const response = await api_gateway.post(`/attendance/confirm/`, body,
             { params: {capacity : query.capacity, event_name : query.event_name, date : query.date,
-                location : query.location, creator_id : query.creator_id } }
-        );            
-        console.log("Error:", response);
+                location : query.location, creator_id : query.creator_id } });
         return response;
     } catch (error) {
         console.error("Fetch error:", error);
